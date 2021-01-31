2ecf2e2a29974f813cab80acf0a8f12009e1e8be
kriolyth <kriolyth@gmail.com>
2021-01-31 01:31:33 +0000

    Deploying to gh-pages from  @ 0cb182ccaf209b2efdedf7c709dee544f90a9f4a 🚀

--- bootstrap.js
+++ bootstrap.js
@@ -371,7 +371,7 @@ module.exports = PIXI;
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"index_ts":{"../pkg/valo_bg.wasm":"8855ec6f650439d225ed"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"index_ts":{"../pkg/valo_bg.wasm":"eaf0931e9220359cc533"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
